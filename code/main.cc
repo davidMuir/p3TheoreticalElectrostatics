6c834c61a549d4c61dc2585cb71c694374317488
JuliaKettle <1003354k@student.gla.ac.uk>
2013-02-19 14:18:49 +0000

    Actually added the file this time

--- code/main.cc
+++ code/main.cc
@@ -13,29 +13,43 @@ int main() {
 	Grid first_grid(n,m);
 
 	first_grid.set_flow(50, -50);
-	first_grid.set_boundary_shape(25,25,10,0,0,circle);
+	first_grid.set_boundary_shape(50,50,10,0,0,circle);
 
 	Finite_Difference fd (first_grid);
 	fd.set_precision(0.00001);
 	fd.set_maxit(10000);
 
-	Grid sol = fd.get_solution();
+	Grid sol_fd = fd.get_solution();
 	//cout << fd.get_iterations() << endl;
-	sol.efield();
+	sol_fd.efield();
 
-	Grid comp = find_err(sol , first_grid);
+	Asymmetric_Finite_Volume fv(first_grid);
+	Grid sol_fv = fv.get_solution();
 
-	comp.print_gnuplot_values();
+	Grid entry(m,n);
+	entry.set_flow(50, -50);
+	Analytic an(entry);
+	an.solve(50,50,20,0,1);
+	Grid analytic = an.get_solution();
+
+	Grid comp_fd = find_err(analytic , sol_fd);
+	Grid comp_fv = find_err(analytic, sol_fv);
+
+
+	comp_fd.print_matrix_to("fd_err.dat");
+	comp_fv.print_matrix_to("fv_err.dat");
 
 	//	sol.print_gnuplot_values();
-	sol.print_matrix_to("matrix_circle_fd.dat");
-	sol.print_all_to("all_circle_fd.dat");
+	//	sol_fd.print_matrix_to("matrix_circle_fd.dat");
+	//	sol.print_all_to("all_circle_fd.dat");
+
+
 	
 //	sol.print_gnuplot_values();
 //	sol.print_matrix_to("matrix_circle_fd.dat");
 //	sol.print_all_to("all_circle_fd.dat");
 
-	Gnuplot gp(sol);
+	Gnuplot gp(sol_fd);
 	gp.add_comment("testing comment");
 	gp.add_command("set term jpeg");
 	gp.add_command("set output 'test.jpg'");
@@ -43,13 +57,13 @@ int main() {
 	gp.save_string("foo.gnu");
 	gp.sendString();
 
-	Gnuplot rf(sol);
+	Gnuplot rf(sol_fd);
 	rf.read_file("foo.gnu");
 	rf.save_string("bar.gnu");
 
-	sol.print_gnuplot_values();
-	sol.print_matrix_to("matrix_circle_fd.dat");
-	sol.print_all_to("all_circle_fd.dat");
+	//	sol.print_gnuplot_values();
+	//	sol.print_matrix_to("matrix_circle_fd.dat");
+	//	sol.print_all_to("all_circle_fd.dat");
 	
 	return 0;
 

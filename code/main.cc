4e6e89baaf630e61013915819dc8386533de380b
JuliaKettle <1003354k@student.gla.ac.uk>
2013-02-20 14:58:40 +0000

    Just added a function to find average value of a grid to Utils called average_grid

--- code/main.cc
+++ code/main.cc
@@ -2,6 +2,7 @@
 #include "Utils/Gnuplot.h"
 #include "Algorithms/Finite_Difference.h"
 #include "Errors/Compare.h"
+#include "Algorithms/Fast_Finite_Difference.h"
 
 using namespace std;
 
@@ -15,6 +16,12 @@ int main() {
 	first_grid.set_flow(50, -50);
 	first_grid.set_boundary_shape(50,50,10,0,0,circle);
 
+	Grid entry(n,m);
+	entry.set_flow(50, -50);
+	Analytic an(entry);
+	an.solve(50,50,10,0,1);
+	Grid analytic = an.get_solution();
+
 	Finite_Difference fd (first_grid);
 	fd.set_precision(0.00001);
 	fd.set_maxit(10000);
@@ -23,21 +30,39 @@ int main() {
 	//cout << fd.get_iterations() << endl;
 	sol_fd.efield();
 
+	Grid comp_fd = find_err(analytic , sol_fd);
+	comp_fd.print_matrix_to("fd_err.dat");
+	cout << "average error in finite diff " << average_grid(comp_fd) << endl;
+	Fast_Finite_Difference ffd(first_grid);
+	ffd.set_precision(0.00001);
+	ffd.set_maxit(10000);
+	Grid sol_ffd = ffd.get_solution();
+	Grid comp_ffd = find_err(analytic, sol_ffd);
+	comp_ffd.print_matrix_to("ffd_err.dat");
+	cout << "average error fast fd " << average_grid(comp_ffd) << endl;
+
 	Asymmetric_Finite_Volume fv(first_grid);
+	fv.set_precision(0.00001);
+	fv.set_maxit(10000);
 	Grid sol_fv = fv.get_solution();
 
-	Grid entry(m,n);
-	entry.set_flow(50, -50);
-	Analytic an(entry);
-	an.solve(50,50,20,0,1);
-	Grid analytic = an.get_solution();
 
-	Grid comp_fd = find_err(analytic , sol_fd);
+
+
+
+
+
 	Grid comp_fv = find_err(analytic, sol_fv);
 
 
-	comp_fd.print_matrix_to("fd_err.dat");
+
 	comp_fv.print_matrix_to("fv_err.dat");
+
+
+	cout << "average error finite vol " << average_grid(comp_fv) << endl;
+
+
+
 
 	//	sol.print_gnuplot_values();
 	//	sol_fd.print_matrix_to("matrix_circle_fd.dat");
@@ -49,7 +74,7 @@ int main() {
 //	sol.print_matrix_to("matrix_circle_fd.dat");
 //	sol.print_all_to("all_circle_fd.dat");
 
-	Gnuplot gp(sol_fd);
+/*	Gnuplot gp(sol_fd);
 	gp.add_comment("testing comment");
 	gp.add_command("set term jpeg");
 	gp.add_command("set output 'test.jpg'");
@@ -60,7 +85,7 @@ int main() {
 	Gnuplot rf(sol_fd);
 	rf.read_file("foo.gnu");
 	rf.save_string("bar.gnu");
-
+*/
 	//	sol.print_gnuplot_values();
 	//	sol.print_matrix_to("matrix_circle_fd.dat");
 	//	sol.print_all_to("all_circle_fd.dat");

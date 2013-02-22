e14a119d23bcf063138e13fe4a53cfc8402711ca
knordstrom <1003412n@student.gla.ac.uk>
2013-02-22 16:34:08 +0000

    Multiple conductors working. New programs folder.

--- code/Utils/Utils.h
+++ code/Utils/Utils.h
@@ -48,6 +48,17 @@ inline double average_grid(Grid entry){
   return total / (mat.size()*mat[0].size());
 }
 
+inline double sum_grid(Grid entry) {
+	matrix mat = entry.get_values();
+	double total = 0;
+	for (int i = 0; i< mat.size(); i++){
+		for (int j = 0; j< mat[0].size(); j++){
+			total += mat[i][j].value;
+		}
+	}
+	return total;
+}
+
 
 
 #endif /* UTILS_H_ */
